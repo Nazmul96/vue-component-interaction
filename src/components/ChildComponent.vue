6f234eebfaf27a32d588e9ec8f1f8286e30422c2
Nazmul Hossain <hossainnazmul191@gmail.com>
2023-08-20 21:30:33 +0600

    module 8 assignment done

--- src/components/ChildComponent.vue
+++ src/components/ChildComponent.vue
@@ -1,7 +1,13 @@
 <script setup>
-
+ defineProps(['modelValue'])
+ defineEmits(['update:modelValue'])
 </script>
 <template>
-       <h3>Child Component</h3>
-
+       <h3 class="text-blue-700 text-xl my-5">Child Component</h3>
+       <p class="my-5">
+         {{ modelValue }}
+      </p>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-5 w-48" @click="$emit('update:modelValue', 'Text change from child')">
+         Button
+       </button>
 </template>
--- src/components/ParentComponent.vue
+++ src/components/ParentComponent.vue
@@ -1,10 +1,13 @@
 <script setup>
-      import ChildComponent from './components/ChildComponent.vue'  
+      import ChildComponent from './ChildComponent.vue'  
       import { ref } from 'vue';
       const message  = ref("Hello from parent!");
 </script>
 
 <template>
-    <h2>Parent Component</h2>
-    <ChildComponent/>
+    <h2 class="text-blue-700 text-xl my-5">Parent Component</h2>
+    <p class="my-5">
+        {{ message }}
+    </p>
+    <ChildComponent v-model="message"/>
 </template>
